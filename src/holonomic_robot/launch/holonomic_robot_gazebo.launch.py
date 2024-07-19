b0cd13a5bda7abba23ec1414a440adc6f4a3771e
DeylaHdz <deylahdzm11@gmail.com>
2024-07-19 11:08:19 -0500

    Fix 1

--- src/holonomic_robot/launch/holonomic_robot_gazebo.launch.py
+++ src/holonomic_robot/launch/holonomic_robot_gazebo.launch.py
@@ -1,38 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-import os
 
 def generate_launch_description():
-    package_share_directory = os.path.join(
-        os.getenv('AMENT_PREFIX_PATH').split(':')[0],
-        'share/holonomic_robot'
-    )
-
-    urdf_file = os.path.join(package_share_directory, 'urdf', 'holonomic_robot.urdf')
-
     return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'
-        ),
         Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=['-entity', 'holonomic_robot', '-file', urdf_file],
-            output='screen'
-        ),
-        Node(
-            package='holonomic_robot',  # Asegúrate de que este sea el nombre correcto del paquete
+            package='occupancy_grid_demo',
             executable='occupancy_grid_publisher',
             name='occupancy_grid_publisher',
             output='screen'
         ),
         Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_entity',
             output='screen',
-            arguments=['-d', os.path.join(os.getenv('AMENT_PREFIX_PATH').split(':')[0], 'share/holonomic_robot/default.rviz')]
+            arguments=[
+                '-entity', 'holonomic_robot',
+                '-file', '/home/deylahm/Desktop/Simulacion Rotbot/Prueba-Ros2/install/holonomic_robot/share/holonomic_robot/urdf/holonomic_robot.urdf'
+            ]
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='gazebo',
+            name='gazebo',
+            output='screen',
+            arguments=[
+                '--verbose',
+                '-s', 'libgazebo_ros_init.so',
+                '-s', 'libgazebo_ros_factory.so'
+            ]
         )
     ])
